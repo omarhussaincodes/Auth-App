4c788a885295191e79e156de5c845213fa1ee49c
omar hussain <omarhussain23moh@gmail.com>
2023-02-26 15:08:18 -0400

    project layout clientside

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+      </header>
+      <main>
+        <h1>Auth App</h1>
+      </main>
+    </div>
+  );
+}
+
+export default App;
